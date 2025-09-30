3a347514188f8acf28d707df81e34e2a1f62b905
Syofyan Zuhad <mail@syofyanzuhad.dev>
2025-09-30 19:45:24 +0700

    feat(routes): redirect root URL to admin panel and comment out welcome view

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::redirect('/', '/admin');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
