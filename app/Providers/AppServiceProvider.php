aae4382ee8caff9a3175228f7350baa822fe604a
Syofyan Zuhad <mail@syofyanzuhad.dev>
2025-09-30 19:16:12 +0700

    feat(provider): register FeedRecordObserver in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\FeedRecord;
+use App\Observers\FeedRecordObserver;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        FeedRecord::observe(FeedRecordObserver::class);
     }
 }
